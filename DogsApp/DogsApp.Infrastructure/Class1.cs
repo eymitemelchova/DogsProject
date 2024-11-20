6d759c4ce3d6a3cc9b3bfc9309af45eee21990a0
eymitemelchova <e.temelchova2020@pmg-pernik.com>
2024-11-20 09:40:27 +0200

    First commit

---
+++ DogsApp/DogsApp.Infrastructure/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace DogsApp.Infrastructure
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ DogsApp/DogsApp/Controllers/DogController.cs
@@ -0,0 +1,93 @@
+﻿using DogsApp.Infrastructure.Data;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DogsApp.Controllers
+{
+    public class DogController : Controller
+    {
+        // GET: DogController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: DogController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: DogController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DogController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DogController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: DogController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DogController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: DogController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        
+        
+            private readonly ApplicationDbContext _context;
+            public DogController(ApplicationDbContext context)
+            {
+                _context = context;
+            }
+        
+    }
+}
